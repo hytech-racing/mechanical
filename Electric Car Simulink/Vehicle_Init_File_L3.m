d2a93e2317f7cd65c5d41fb4536b64376f46b563
wli450 <55769447+wli450@users.noreply.github.com>
2019-10-08 19:44:47 -0400

    Update Vehicle_Init_File_L3.m
    Updated various measurments of different parameters such a little sprockets inertia or half shaft inertia, also updated the summation of all of the inertias in the equivalent drivetrain inertia

--- Electric Car Simulink/Vehicle_Init_File_L3.m
+++ Electric Car Simulink/Vehicle_Init_File_L3.m
@@ -18,24 +18,25 @@ Vehicle_Tire_Radius = 0.2286;       % Tire radius, [m] (9 inch)
 Wheel_Inertia = 0.02179531;         % Wheel Inertia, [kg-m^2]
 Tire_Inertia = 0.193286;            % Tire Inertia, [kg-m^2]
 Wheel_Plus_Tire_Inertia = Wheel_Inertia + Tire_Inertia;         % Combined Wheel-Tire Inertia, [kg-m^2]
-Vehicle_DShaft_Inertia = 0.00000646177;% Halfshaft Inertia, [kg-m^2]
+Vehicle_DShaft_Inertia = 0.00004717091;% Halfshaft Inertia, [kg-m^2]
 
 %Drivetrain Parameters
+%Most Masses were estimate using the same density of alloy steel
 Big_Sprocket_Inertia = 0.0013908562;            % Big Sprocket Inertia, [kg-m^2]
-Little_Sprocket_Inertia = 0.00000291714;        % Little Sprocket Inertia, [kg-m^2]
+Little_Sprocket_Inertia = 0.00002132185;        % Little Sprocket Inertia, [kg-m^2]
 Differential_Inertia = 0.0021039697;            % Differential Inertia, [kg-m^2]
 Inner_Piece_Bearings_Inertia = 0.00007933483;   % Inner Piece of Bearings Inertia, [kg-m^2]
-Sprocket_Interface_Inertia = 0.00008432517;     % Sprocket Interface Inertia, [kg-m^2]
+Sprocket_Interface_Inertia = 0.0002190654;     % Sprocket Interface Inertia, [kg-m^2]
 Rear_Sprocket_Teeth = 41;                       % Teeth on Front Sprocket, [-]
 Front_Sprocket_Teeth = 8;                       % Teeth on Rear Sprocket, [-]
 
 %Motor Parameters
 load Component_Data/MG_Data_Continuous.mat;
-Motor_Output_Shaft_Inertia = 0.0006116169;  % Motor Output Shaft Inertia, [kg-m^2]
+Motor_Output_Shaft_Inertia = 0.0011480592;  % Motor Output Shaft Inertia, [kg-m^2]
 Emrax188_Rotor_Inertia = 0.0134;            % Emrax 188 Rotor Inertia, [kg-m^2]
 Emrax208_Rotor_Inertia = 0.0256;                  % Emrax 208 Rotor Inertia, [kg-m^2]
 Rotor_Inertia = Emrax188_Rotor_Inertia;     % Sprocket Interface Inertia, [kg-m^2]
-Equivalent_Drivetrain_Intertia = sum(Big_Sprocket_Inertia, Little_Sprocket_Inertia, Differential_Inertia, Inner_Piece_Bearings_Inertia, Sprocket_Interface_Inertia, Motor_Output_Shaft_Inertia, Emrax208_Rotor_Inertia);
+Equivalent_Drivetrain_Intertia = sum(Vehicle_DShaft_Inertia, Tire_Inertia, Wheel_Inertia, Big_Sprocket_Inertia, Little_Sprocket_Inertia, Differential_Inertia, Inner_Piece_Bearings_Inertia, Sprocket_Interface_Inertia, Motor_Output_Shaft_Inertia, Emrax208_Rotor_Inertia);
 
 %Battery Parameters
 load Component_Data/Battery_Data.mat; 

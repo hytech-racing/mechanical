faa43f56591940d22725c559033eb2de8bccb8a1
mattjweist <mattjweist@gmail.com>
2019-11-07 21:51:00 -0500

    Motor Speed as input; 208 data
    Modified simulink and init file to include motor speed data as input; added 208 motor data

--- Electric Car Simulink/Vehicle_Init_File_L3.m
+++ Electric Car Simulink/Vehicle_Init_File_L3.m
@@ -9,7 +9,7 @@ load Drive_Cycles/Schedule_Autocross_1km.mat;
 %Sch_Cycle
 
 %Vehicle Parameters
-Vehicle_Mass = 241;                 % Vehicle Mass with Driver, [kg]
+Vehicle_Mass = 234;                 % Vehicle Mass with Driver, [kg]
 Vehicle_CG_in = 8;                  % Vehicle CG, [in]
 Vehicle_FrontToCG_in = 19;          % Vehicle Front Axle to CG, [in]
 Vehicle_RearToCG_in = 34.6;         % Vehicle Rear Axle to CG, [in]
@@ -29,11 +29,11 @@ Little_Sprocket_Inertia = 0.00002132185;        % Little Sprocket Inertia, [kg-m
 Differential_Inertia = 0.0021039697;            % Differential Inertia, [kg-m^2]
 Inner_Piece_Bearings_Inertia = 0.00007933483;   % Inner Piece of Bearings Inertia, [kg-m^2]
 Sprocket_Interface_Inertia = 0.0002190654;     % Sprocket Interface Inertia, [kg-m^2]
-Rear_Sprocket_Teeth = 41;                       % Teeth on Front Sprocket, [-]
+Rear_Sprocket_Teeth = 40;                       % Teeth on Front Sprocket, [-]
 Front_Sprocket_Teeth = 8;                       % Teeth on Rear Sprocket, [-]
 
 %Motor Parameters
-load Component_Data/MG_Data_Continuous.mat;
+load Component_Data/208/MG_Data_Continuous.mat;
 Motor_Output_Shaft_Inertia = 0.0011480592;  % Motor Output Shaft Inertia, [kg-m^2]
 Emrax188_Rotor_Inertia = 0.0134;            % Emrax 188 Rotor Inertia, [kg-m^2]
 Emrax208_Rotor_Inertia = 0.0256;            % Emrax 208 Rotor Inertia, [kg-m^2]
@@ -66,3 +66,4 @@ battery_soc_high = 0.9;             % Battery SOC High Value, [Decimal]
 Logging_Sample_Time_s = 0.01;          %Logging time, [s]
 
 load Component_Data/Commanded_Torque.mat;
+load Component_Data/motor_speed_data.mat;
